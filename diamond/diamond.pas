0b65d091570e8c571ad07792ac9d9851012352fb
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-01-08 20:05:10 +0300

    add program diamon

---
+++ diamond/diamond.pas
@@ -0,0 +1,36 @@
+program diamond;
+
+var n, k, h, i: integer;
+
+begin
+    repeat
+        write('Enter the diamond''s height (possitive odd): ');
+        readln(h);
+    until (h > 0) and (h mod 2 = 1);
+
+    n := h div 2;
+
+    for k := 1 to n + 1 do begin
+        for i := 1 to n + 1 -k do
+            write(' ');
+        write('*');
+        if k > 1 then begin
+            for i := 1 to 2 * k - 3 do
+                write(' ');
+            write('*');
+        end;
+        writeln;
+    end;
+
+    for k := n downto 1 do begin
+        for i := 1 to n + 1 - k do
+            write(' ');
+        write('*');
+        if k > 1 then begin
+            for i := 1 to 2 * k - 3 do
+                write(' ');
+            write('*');
+        end;
+        writeln;
+    end;
+end.
