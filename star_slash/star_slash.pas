3d5cbc15e514fcfa011aed26e244b0df13542a45
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-01-15 13:37:06 +0300

    change star_slash

--- star_slash/star_slash.pas
+++ star_slash/star_slash.pas
@@ -1,11 +1,12 @@
 program star_slash;
 
-var n, m: integer;
+var
+    n, m: integer;
 
 begin
     for n := 1 to 24 do begin
         for m := 1 to n - 1 do
             write(' ');
         writeln('*');
-    end
+    end;
 end.
