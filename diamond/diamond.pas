585ef12fa029fe44ec8b3ecb414daf8d94098183
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-01-20 23:42:09 +0300

    change diamond

--- diamond/diamond.pas
+++ diamond/diamond.pas
@@ -1,36 +1,36 @@
 program diamond;
 
-var n, k, h, i: integer;
+var half, j, height, i: integer;
 
 begin
     repeat
         write('Enter the diamond''s height (possitive odd): ');
-        readln(h);
-    until (h > 0) and (h mod 2 = 1);
+        readln(height);
+    until (height > 0) and (height mod 2 = 1);
 
-    n := h div 2;
+    half := height div 2;
 
-    for k := 1 to n + 1 do begin
-        for i := 1 to n + 1 -k do
+    for j := 1 to half + 1 do begin
+        for i := 1 to half + 1 - j do
             write(' ');
         write('*');
-        if k > 1 then begin
-            for i := 1 to 2 * k - 3 do
+        if j > 1 then begin
+            for i := 1 to 2 * j - 3 do
                 write(' ');
             write('*');
         end;
-        writeln;
+        writeln();
     end;
 
-    for k := n downto 1 do begin
-        for i := 1 to n + 1 - k do
+    for j := half downto 1 do begin
+        for i := 1 to half + 1 - j do
             write(' ');
         write('*');
-        if k > 1 then begin
-            for i := 1 to 2 * k - 3 do
+        if j > 1 then begin
+            for i := 1 to 2 * j - 3 do
                 write(' ');
             write('*');
         end;
-        writeln;
+        writeln();
     end;
 end.
