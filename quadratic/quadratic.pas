4e1c276a3a4cdc0c4cc05fe17de045ca66211b68
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-01-29 16:43:07 +0300

    add quadratic

---
+++ quadratic/quadratic.pas
@@ -0,0 +1,17 @@
+procedure quadratic(a, b, c: real;
+                    var ok: boolean;
+                    var x1, x2: real);
+var
+    d: real;
+begin
+    ok := false;
+    if a = 0 then
+        exit();
+    d := b * b - 4 * a * c;
+    if d < 0 then
+        exit();
+    d := sqrt(d);
+    x1 := (-b - d) / (2 * a);
+    x1 := (-b + d) / (2 * a);
+    ok = true;
+end;
