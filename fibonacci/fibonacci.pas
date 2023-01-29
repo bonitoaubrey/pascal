70244cb8ec76ac40b39faed3131f0d88a851a116
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-01-29 16:26:12 +0300

    change fibonacci

--- fibonacci/fibonacci.pas
+++ fibonacci/fibonacci.pas
@@ -3,16 +3,19 @@ var
     i: integer;
     previous, current, res: longint;
 begin
-    if number <= 0 then
-        fibonacci := 0
-    else begin
-        current := 0;
-        res := 1;
-        for i := 2 to number do begin
-            previous := current;
-            current := res;
-            res := previous + current;
-        end;
-        fibonacci := res;
+    if number <= 0 then begin
+        fibonacci := 0;
+        exit();
     end;
+
+    current := 0;
+    res := 1;
+
+    for i := 2 to number do begin
+        previous := current;
+        current := res;
+        res := previous + current;
+    end;
+
+    fibonacci := res;
 end;
