fbbfa146d399bffd48f9fdf38f174dc080effd8e
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-02-04 23:44:14 +0300

    add simple_sum

---
+++ simple_sum/simple_sum.pas
@@ -0,0 +1,16 @@
+program simple_sum;
+
+var
+    sum, count, n: longint;
+
+begin
+    sum := 0;
+    count := 0;
+    while not SeekEof do begin
+        read(n);
+        sum := sum + n;
+        count := count + 1;
+    end;
+
+    writeln(count, ' ', sum);
+end.
