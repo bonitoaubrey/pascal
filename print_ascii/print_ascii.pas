03c85a0ec5976b44e105a0751a12c9d404a7f5e3
Bonito Aubrey <bonitoaubrey@gmail.com>
2023-02-05 14:08:42 +0300

    change print_ascii

--- print_ascii/print_ascii.pas
+++ print_ascii/print_ascii.pas
@@ -11,10 +11,12 @@ begin
     for c := 'A' to 'F' do
         write(' .', c);
     writeln();
+
     write('  |');
     for i := 1 to 16 do
         write('---');
     writeln();
+
     for i := 2 to 7 do begin
         write(i, '.|');
         for j := 0 to 15 do
